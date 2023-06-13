a64290ab009e0f8c49c1e10911284d428c8fcd47
saed <saedabdi3@gmail.comm>
2023-06-13 13:38:06 -0400

    work

--- express_server.js
+++ express_server.js
@@ -1,42 +1,69 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
-// Middleware to parse request body
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
-// Database for storing URLs
-const urlDatabase = {};
+// Sample data storage
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-// Function to generate random string
-function generateRandomString() {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const length = 6;
+// Home page route
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters.charAt(randomIndex);
-  }
+// URL Index page route
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-  return result;
-}
-
-// Route to render new URL form
+// New URL submission form route
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// Route to handle form submission and create new URL
+// URL details page route
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+// URL submission form submission route
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
+
+  // Log the request body
   console.log(req.body);
-  res.send("Ok");
+
+  res.redirect(`/urls/${shortURL}`);
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
+
+// Helper function to generate random string for short URL
+function generateRandomString() {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return result;
+}
