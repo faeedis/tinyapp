b9cc8e889bc5b308a084c3da386df10f63593ae0
saed <saedabdi3@gmail.comm>
2023-06-12 20:46:18 -0400

    Updated URLs templates

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -13,13 +13,26 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const shortURLId = req.params.id;
-  const longURL = urlDatabase[shortURLId];
-  const templateVars = { shortURLId, longURL };
-  res.render("urls_show", templateVars);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { id, longURL };
+  res.render("urls_show", templateVars);
 });
