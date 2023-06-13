d4ad5c13accb47fbf30ea3746f47108fd3ae65f7
saed <saedabdi3@gmail.comm>
2023-06-12 21:47:23 -0400

    Add form submission route

--- express_server.js
+++ express_server.js
@@ -1,38 +1,42 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
-app.set("view engine", "ejs");
+// Middleware to parse request body
+app.use(express.urlencoded({ extended: true }));
 
-const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+// Database for storing URLs
+const urlDatabase = {};
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// Function to generate random string
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 6;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+// Route to render new URL form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+// Route to handle form submission and create new URL
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  console.log(req.body);
+  res.send("Ok");
 });
 
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
-  res.render("urls_show", templateVars);
+  console.log(`TinyApp listening on port ${PORT}`);
 });
